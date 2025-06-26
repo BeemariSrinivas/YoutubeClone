88c974bbafc24b194f1a50c98cfbb0154d5f1bfd
BeemariSrinivas <beemarisrinivas528@gmail.com>
2025-06-26 08:09:21 +0530

    Fourth Commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,18 +1,19 @@
-import { StrictMode } from 'react'
+import { lazy, StrictMode, Suspense } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import NotFound from './Components/NotFoundPage.jsx'
-import HomePage from './Components/HomePage.jsx'
-import UserAuthenticationPage from './Components/UserAuthenticationPage.jsx'
-import UserRegistrationPage from './Components/UserRegistrationPage.jsx'
-import CreateChannelPage from './Components/CreateChannelPage.jsx'
-import ChannelPage from './Components/ChannelPage.jsx'
 import { UserProvider } from './Components/UserContex.jsx'
-import UploadVideoPage from './Components/UploadVideoPage.jsx'
-import VideoPlayerPage from './Components/VideoPlayerPae.jsx'
-import VideoEditPage from './Components/VideoEditPage.jsx'
+
+const NotFound = lazy(()=>import('./Components/NotFoundPage.jsx'));
+const HomePage = lazy(()=>import('./Components/HomePage.jsx'));
+const UserAuthenticationPage = lazy(()=>import('./Components/UserAuthenticationPage.jsx'));
+const UserRegistrationPage = lazy(()=>import('./Components/UserRegistrationPage.jsx'))
+const CreateChannelPage = lazy(()=>import('./Components/CreateChannelPage.jsx'));
+const ChannelPage = lazy(()=>import('./Components/ChannelPage.jsx'));
+const UploadVideoPage = lazy(()=>import('./Components/UploadVideoPage.jsx'));
+const VideoPlayerPage = lazy(()=>import('./Components/VideoPlayerPae.jsx'));
+const VideoEditPage = lazy(()=>import('./Components/VideoEditPage.jsx'));
 
 //app routes manages all the routes in the app
 const appRouter = createBrowserRouter([
@@ -61,7 +62,9 @@ const appRouter = createBrowserRouter([
 createRoot(document.getElementById('root')).render(
     <UserProvider>
       <StrictMode>
-        <RouterProvider router={appRouter}/>
+        <Suspense fallback={<div style={{"textAlign":"center","paddingTop":"50px"}}>Loading......</div>}>
+          <RouterProvider router={appRouter}/>
+        </Suspense>
       </StrictMode>
     </UserProvider>
 )
